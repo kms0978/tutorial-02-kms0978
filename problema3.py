0bd238c2e5b9b227c0a400032af54de9ee93eb8e
kms0978 <114182131+kms0978@users.noreply.github.com>
2024-03-22 12:48:58 -0300

    solución problema3

--- problema3.py
+++ problema3.py
@@ -9,5 +9,39 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+
+
+def insertar_arbol(arbol, numero):
+    if not arbol:
+        return [numero, [], [], []]
+    elif numero == arbol[0]:
+        if isinstance(arbol[2], list) and not arbol[2]: 
+            arbol[2] = [numero, [], [], []]
+        elif numero not in arbol[2]:
+            arbol[2] = insertar_arbol(arbol[2], numero)
+    elif numero < arbol[0]:
+        arbol[1] = insertar_arbol(arbol[1], numero)
+    else:
+        arbol[3] = insertar_arbol(arbol[3], numero)
+    return arbol
+
+def imprimir_arbol(arbol):
+    if not arbol:
+        return
+    if isinstance(arbol, int):
+        print(arbol, end=" ")
+        return
+    print("[", arbol[0], end=" ")
+    for i in range(len(arbol)):
+      if arbol[i]:
+          imprimir_arbol(arbol[i])
+      else:
+          print("[]", end=" ")
+    print("]", end="")
+
+t = input().split()
+arbol = None
+for numero in t:
+    arbol = insertar_arbol(arbol, int(numero))
+
+imprimir_arbol(arbol)
